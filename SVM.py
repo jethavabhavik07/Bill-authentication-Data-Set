22df0f2cb7ca9e146ca41191b7cbcc0f5b51ca99
mca <mca@ibmserver.local>
2019-03-09 12:12:01 +0530

    first commit

---
+++ SVM.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  9 11:03:30 2019
+
+@author: mca
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+bankdata = pd.read_csv("E:\\bill_authentication.csv")
+X = bankdata.drop('Class',axis = 1)
+y = bankdata['Class']
+
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.2)
+
+from sklearn.svm import SVC
+
+svclassifier = SVC(kernel='rbf')
+svclassifier.fit(X_train,y_train)
+
+y_pred = svclassifier.predict(X_test)
+from sklearn.metrics import classification_report,confusion_matrix
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
+ 
